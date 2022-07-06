3f01afa3f7a1bf0e837e03e6ba99f12e7873f2bb
diamant3 <diamant3@zohomail.com>
2022-07-06 10:38:39 +0800

    complete input keys

--- bytepusher.lua
+++ bytepusher.lua
@@ -8,7 +8,25 @@ Bytepusher.__index = Bytepusher
 function Bytepusher.new()
     return setmetatable({
         mem = {},
-        pc = 0x0
+        pc = 0x0,
+        keyboard = {
+            ["1"] = 0x1,
+            ["2"] = 0x2,
+            ["3"] = 0x3,
+            ["4"] = 0xc,
+            ["q"] = 0x4,
+            ["w"] = 0x5,
+            ["e"] = 0x6,
+            ["r"] = 0xd,
+            ["a"] = 0x7,
+            ["s"] = 0x8,
+            ["d"] = 0x9,
+            ["f"] = 0xe,
+            ["z"] = 0xa,
+            ["x"] = 0x0,
+            ["c"] = 0xb,
+            ["v"] = 0xf
+        }
     }, Bytepusher)
 end
 
